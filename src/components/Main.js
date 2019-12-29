f54ac21cd693514d00e5fbc45a2c8d2b6e239a18
Alvin Nguyen <alvinnguyen726@gmail.com>
2019-12-29 10:42:17 -0600

    first commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,12 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import consultation from '../images/consultation.jpg'
+import design from '../images/design.jpg'
+import development from '../images/development.jpg'
+import data from '../images/data.jpg'
+import iteration from '../images/iteration.jpg'
+
 
 class Main extends React.Component {
   render() {
@@ -22,65 +28,143 @@ class Main extends React.Component {
         style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
       >
         <article
-          id="intro"
-          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+          id="services"
+          className={`${this.props.article === 'service' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
+          <h2 className="major">Services</h2>
+
+          <h3>Consultation</h3>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={consultation} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+          Where do you want to go? How do we get you there? Which technologies and digital solutions?
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+          CentiDigital will walk you through the process from the beginning to launch. We show you the opportunities for growth and risks in the latest technologies. We have a deep understanding of the advantages and disadvantages of different system architectures and will support you in selecting the best digital solutions for your business.
           </p>
+
+          <h3>Design</h3>
+          <span className="image main">
+            <img src={design} alt="" />
+          </span>
+          <p>
+          We understand people are visual creatures. Visual design has a significant impact on how we understand products and services. The importance of effective and captivating UX and UI design cannot be overstated. 
+          </p>
+          <p>
+          At CentiDigital, we implement modern design techniques that are always responsive and intriguing. We don't throw your ideas into a template like most other digital creators do because we value your brand and business. 
+          </p>
+
+          <h3>Development</h3>
+          <span className="image main">
+            <img src={development} alt="" />
+          </span>
+          <p>
+          We develop your digital solutions using modern technologies and techniques. We are not bound to a specific set of tools, but have extensive capabilities in a vast assortment of technologies. This allows us to choose the right ones that best suit your needs. From web to mobile development, our work is always built for reliability, flexibility, and scale.
+          </p>
+
+          
+          <h3>Tracking &amp; Data Analysis</h3>
+          <span className="image main">
+            <img src={data} alt="" />
+          </span>
+          <p>
+          We understand the importance of evaluating data points to guide the work we do. We use quantitative and qualitative tracking tools to measure your web and mobile app performance, so that we can advise on future ideas and implementations. We are a data and results-driven company that will always be looking for the best usage of data to benefit your business.
+          </p>
+          
+          
+          <h3>Iteration</h3>
+          <span className="image main">
+            <img src={iteration} alt="" />
+          </span>
+          <p>
+          Successful businesses understand the importance of revision and iteration. Digital technologies are improving and growing at a rapid pace. CentiDigital is at the forefront of design and development trends. We can assure you that your digital solutions will always be current and relevant.
+          </p>
+
           {close}
         </article>
 
         <article
-          id="work"
-          className={`${this.props.article === 'work' ? 'active' : ''} ${
+          id="process"
+          className={`${this.props.article === 'process' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Work</h2>
+          <h2 className="major">Process</h2>
+
+          <p>
+          We understand that your web and mobile solution reflects your brand. It's where clients begin learning about your taste, attention to detail, and business philosophy - it's the most significant point of contact for any potential client's journey. Your revenue will grow if you truly understand your audience and create a smooth and engaging process for them. 
+          </p>
+          <p>
+          CentiDigital's strategic model is a 7-step process to ensuring we achieve a clear plan, captivating design, sharp development, and most importantly, business growth.
+          </p>
+
           <span className="image main">
-            <img src={pic02} alt="" />
+            <img src={pic01} alt="" />
           </span>
+          <h3>Learning &amp; Mapping</h3>
+          <h6>01 / 07</h6>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+          Inspired by the renowned Google Ventures Design Sprint methodology, we use our CD Design Model to start learning all about you, your business and your clients with the help of powerful market research and data analysis tools.
           </p>
+
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <h3>UX Design</h3>
+          <h6>02 / 07</h6>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+          We leverage insights from the CD Design Model to plan and design a captivating user experience catered to your client base.
           </p>
+
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <h3>UI Design</h3>
+          <h6>03 / 07</h6>
+          <p>
+          With proven innovative design tools such as Adobe Xd, Figma and Sketch, we collaborate with you to create sophisticated and elegant designs that incorporate the latest trends and capabilities.
+          </p>
+
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <h3>Develop</h3>
+          <h6>04 / 07</h6>
+          <p>
+          CentiDigital uses an agile development methodology to ensure efficient and timely development. With extensive capabilities in a vast assortment of development tools, choosing the right development tools for the job is just as important as programming effective and dynamic code. We don't use cheap, bloated templates that affect performance and optimization. Our developers create components that are catered to your specific web and mobile solutions.
+          </p>
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <h3>Test</h3>
+          <h6>05 / 07</h6>
+          <p>
+          We believe testing is one of the most important and overlooked steps in creating a high quality product. That is why our QA consists of testing for performance, speed, optimization and accessibility using industry standard reports and metrics. 
+          </p>
+
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <h3>Deploy</h3>
+          <h6>06 / 07</h6>
+          <p>
+          Going live is arguably the most exciting part of this entire process but can also be the most stressful. Whether it is getting setup with hosting or submitting to the app store, we are here to help you along the way to getting published, so you can start monetizing.
+          </p>
+
+          <span className="image main">
+            <img src={pic01} alt="" />
+          </span>
+          <h3>Track &amp; Learn</h3>
+          <h6>07 / 07</h6>
+          <p>
+          Deployment is always followed by measurement and iteration. Using quantitative and qualitative tracking tools, we help you identify opportunities to improve and update based on data analytics. Our motto is: “Always Getting Better”, and that’s what we want for our clients, too.
+          </p>
+          
           {close}
         </article>
 
@@ -92,18 +176,16 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">About</h2>
+          <p>
+          CentiDigital is a digital product development agency based in Dallas, TX.  We deliver impactful customer experiences driven by innovation and insight. 
+          </p>
+          <p>Using a a data-driven approach, our company works with all industries and oversees every step of design &amp; development, from strategy to execution to optimization.</p>
           <span className="image main">
             <img src={pic03} alt="" />
           </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
+          <h3>Our Mission</h3>
+          <p>We believe building meaningful and engaging digital experiences is not only about hiring the best agency, but also about choosing the one that will be able to make the most of your idea and will push it further than you could have imagined. </p>
+
           {close}
         </article>
 
@@ -140,26 +222,26 @@ class Main extends React.Component {
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/CentiDigital" target="_blank"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
+              <a href="https://facebook.com/CentiDigital" target="_blank" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
+              <a href="https://instagram.com/CentiDigital" target="_blank" className="icon fa-instagram">
                 <span className="label">Instagram</span>
               </a>
             </li>
             <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
-                className="icon fa-github"
+                href="mailto:alvin@centidigital.com"
+                className="icon fa-envelope"
               >
                 <span className="label">GitHub</span>
               </a>
